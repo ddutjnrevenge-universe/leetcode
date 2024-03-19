a79c413fc57497aa3cc7f6308bc94acf959a01dc
Hanh-Nguyen Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-03-19 14:20:52 +0700

    Time: 2 ms (60.45%) | Memory: 41.4 MB (38.44%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int sPointer =0; 
-        int tPointer=0;
-        while(sPointer<s.length() && tPointer <t.length()) {
-            if (s.charAt(sPointer) == t.charAt(tPointer)) {
+        int sPointer =0; //pointer in string s
+        int tPointer=0; //pointer in string t
+        while(sPointer<s.length() && tPointer <t.length()) { //iterate over the two string
+            if (s.charAt(sPointer) == t.charAt(tPointer)) { //check if character in s also exists in t)
                 sPointer++;
             }
             tPointer++;
         }
-        return sPointer == s.length();
+        return sPointer == s.length(); //at the end the sPointer would reach the length of string s if the entire string s is a subsequence of string t
     }
 }
