1fd6a4cca10dc52ec42c1f062db1d414cb058469
Hanh-Nguyen Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-03-21 15:33:49 +0700

    Time: 2152 ms (36.16%) | Memory: 12.6 MB (22.36%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # sum = {}
+        # for i,j in enumerate(nums): 
+        #     complement = target - j
+
+        #     if complement in sum:
+        #             return [sum[complement],i]
+        #     sum[j]=i
+        # return []
+        # another solution
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if (nums[i]+nums[j] == target):
+                    return [i,j]
+        return []
