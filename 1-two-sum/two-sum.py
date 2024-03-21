a351414b5c23f7af0b0d851494224698398cfaee
Hanh-Nguyen Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-03-21 15:34:04 +0700

    Time: 35 ms (86.43%) | Memory: 12.5 MB (78.65%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,17 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # sum = {}
-        # for i,j in enumerate(nums): 
-        #     complement = target - j
+        sum = {}
+        for i,j in enumerate(nums): 
+            complement = target - j
 
-        #     if complement in sum:
-        #             return [sum[complement],i]
-        #     sum[j]=i
-        # return []
-        # another solution
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if (nums[i]+nums[j] == target):
-                    return [i,j]
+            if complement in sum:
+                    return [sum[complement],i]
+            sum[j]=i
         return []
+        # # another solution
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if (nums[i]+nums[j] == target):
+        #             return [i,j]
+        # return []
