a5a9525da9fa745b5f3de6a978c743cadf64bf45
Hanh-Nguyen Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-03-21 16:13:32 +0700

    Time: 18 ms (45.87%) | Memory: 11.6 MB (40.48%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -8,7 +8,6 @@ class Solution(object):
         k = 0
         for i in range(len(nums)):
             if nums[i] != val:
-                # nums[i]=''
                 nums[k] = nums[i]
                 k+=1
         return k
