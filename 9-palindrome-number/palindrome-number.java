f7c6b1be93a6945834467c3562aa836e1f8e471d
Hanh-Nguyen Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-03-25 21:18:36 +0700

    Time: 97 ms (5.60%) | Memory: 44.8 MB (5.08%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x<0 || (x!=0 && x%10==0)) {
+        if (x<0 || (x!=0 && x%10==0)) { // when the number is negative or divisible by 10 -> false
             return false;
         } 
         int reversed_num = 0;
         while (x>reversed_num) {
             reversed_num = reversed_num * 10 + x%10;
+            System.out.println(reversed_num);
             x /= 10;
+            
         }
         return x==reversed_num || x==reversed_num /10;
     }
