800662b4f94159d72e7a1d13c4892317237065bc
Hanh-Nguyen Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-03-30 10:22:52 +0700

    Time: 0 ms (100.00%) | Memory: 43.3 MB (25.60%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -10,9 +10,6 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head==null || head.next==null){
-            return head;
-        }
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode current = dummy;
