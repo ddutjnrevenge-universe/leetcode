f274ade6b9dab9124e117b6cf688d1ba2cbc642c
Hanh-Nguyen Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-04-04 15:32:33 +0700

    Time: 505 ms (58.82%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,8 +1,6 @@
-# Write your MySQL query statement below
-select x,y,z,
-case when z between abs(x-y)+1 and (x+y)-1 then 'Yes'
-when y between abs(x-z)+1 and (x+z)-1 then 'Yes'
-when x between abs(z-y)+1 and (z+y)-1 then 'Yes'
-else 'No'
-end as triangle 
-from Triangle
+SELECT x, y, z,
+       CASE 
+           WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+           ELSE 'No'
+       END AS triangle
+FROM Triangle;
