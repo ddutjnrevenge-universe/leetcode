80a3ab892b57a18b76ea9c205213ffe2f76d94ce
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-04-08 08:59:13 +0700

    Time: 278 ms (23.68%) | Memory: 11.5 MB (78.95%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -4,13 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n<=1:
-            return 0
-        dp = [0] * (n + 1)
-
+        if n <= 1:
+            return  0
+    
+        dp = [0] * (n + 1)  # dp[i] represents minimum steps to get 'A' i times on screen
+        
         for i in range(2, n + 1):
-            dp[i] = i  # At worst, we would copy 'A' once and then paste it i-1 times
-            for j in range(2, i):
+            dp[i] = i  # initialize with worst case scenario
+            
+            for j in range(2, i // 2 + 1):
                 if i % j == 0:
-                    dp[i] = min(dp[i], dp[j] + (i // j))  # If i is divisible by j, we can paste j times
+                    dp[i] = min(dp[i], dp[j] + i // j)
+        
         return dp[n]
