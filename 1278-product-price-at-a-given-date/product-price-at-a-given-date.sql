33ab6702b822568dd7c1a93492c54acc260fde19
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-04-09 12:06:07 +0700

    Time: 1380 ms (16.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,10 @@
+SELECT 
+    product_id,
+    COALESCE(
+        (SELECT new_price 
+         FROM Products 
+         WHERE change_date <= '2019-08-16' AND product_id = p.product_id
+         ORDER BY change_date DESC 
+         LIMIT 1),10) AS price
+FROM
+    (SELECT DISTINCT product_id FROM Products) AS p;
