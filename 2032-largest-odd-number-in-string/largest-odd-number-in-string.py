a89923536bd7334878aaacb1f79acd33b025eac9
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-04-12 12:52:46 +0700

    Time: 55 ms (54.65%) | Memory: 17 MB (43.72%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type num: str
         :rtype: str
         """
-        # while i<len(num):
         for i in range(len(num) - 1, -1, -1):
             if int(num[i]) % 2 != 0:
                 return num[:i+1]
