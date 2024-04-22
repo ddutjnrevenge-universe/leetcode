bfa75c82078bf89fb8672a96f574b56d2a84419c
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-04-22 07:41:10 +0700

    Time: 24 ms (74.42%) | Memory: 11.8 MB (74.89%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return None
+        left, right = 0, len(nums)-1
+        while left<right:
+            mid=(left+right)//2
+            if nums[mid]<nums[mid+1]:
+                left=mid+1
+            else:
+                right = mid
+        return left
