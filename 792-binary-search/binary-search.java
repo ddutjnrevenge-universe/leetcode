4ae9b065cf81ade5fae9904af4058e925c213bf8
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-04-22 07:52:53 +0700

    Time: 1 ms (10.72%) | Memory: 46.3 MB (6.50%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,12 +2,9 @@ class Solution {
     public int search(int[] nums, int target) {
         if (nums==null)
             return -1;
-        int index = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                index = i;
-            }
-        }
-        return index;
+        for (int i=0; i<nums.length; i++)
+            if (nums[i]==target)
+                return i;
+            return -1;
     }
 }
