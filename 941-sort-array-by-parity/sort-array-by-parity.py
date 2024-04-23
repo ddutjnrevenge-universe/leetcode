bfd22fe1f7654d2ab1c8417c5a6ee5b260a57e2c
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-04-23 13:21:51 +0700

    Time: 64 ms (6.99%) | Memory: 12.7 MB (14.09%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if len(nums)<=1:
+            return nums
+        odd=[]
+        even=[]
+        for i in range(len(nums)):
+            if nums[i]%2==0:
+                even.append(nums[i])
+            else:
+                odd.append(nums[i])
+        print(even)
+        print(odd)
+        return even+odd
+
+        
