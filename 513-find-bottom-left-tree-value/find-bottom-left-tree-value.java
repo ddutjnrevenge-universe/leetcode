a4a09b40950c8a8883995e4af7a702c30b0da929
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-05-01 14:53:21 +0700

    Time: 0 ms (100.00%) | Memory: 44.2 MB (78.03%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -14,23 +14,45 @@
  * }
  */
 class Solution {
+    // public int findBottomLeftValue(TreeNode root) {
+    //     if (root==null)
+    //         return -1;
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     queue.offer(root);
+    //     int leftmost = -1;
+
+    //     while (!queue.isEmpty()){
+    //         int size = queue.size();
+    //         leftmost = queue.peek().val;
+    //         for (int i=0; i<size; i++) {
+    //             TreeNode node = queue.poll();
+    //             if (node.left!=null) queue.offer(node.left);
+    //             if (node.right!=null) queue.offer(node.right);
+    //         }
+    //     }
+    //     return leftmost;
+    // }
+    int leftmostValue;
+    int maxDepth;
+
     public int findBottomLeftValue(TreeNode root) {
-        if (root==null)
-            return -1;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        int leftmost = -1;
+        leftmostValue = -1;
+        maxDepth = -1;
+        findBottomLeftValueHelper(root, 0);
+        return leftmostValue;
+    }
 
-        while (!queue.isEmpty()){
-            int size = queue.size();
-            leftmost = queue.peek().val;
-            for (int i=0; i<size; i++) {
-                TreeNode node = queue.poll();
-                if (node.left!=null) queue.offer(node.left);
-                if (node.right!=null) queue.offer(node.right);
+    private void findBottomLeftValueHelper(TreeNode node, int depth) {
+        if (node == null) return;
 
-            }
+        // Update leftmost value and max depth if necessary
+        if (depth > maxDepth) {
+            leftmostValue = node.val;
+            maxDepth = depth;
         }
-        return leftmost;
+
+        // Recur for left and right subtrees
+        findBottomLeftValueHelper(node.left, depth + 1);
+        findBottomLeftValueHelper(node.right, depth + 1);
     }
 }
