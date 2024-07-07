92b3b81dbe7d87b3fef85c34138aab5e7b68f529
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-07-07 12:29:44 +0700

    Time: 278 ms (72.66%) | Memory: 0B (100.00%) - LeetSync

--- 1837-daily-leads-and-partners/daily-leads-and-partners.sql
+++ 1837-daily-leads-and-partners/daily-leads-and-partners.sql
@@ -2,3 +2,4 @@
 select date_id, make_name, COUNT(DISTINCT lead_id) AS unique_leads, COUNT(DISTINCT partner_id) AS unique_partners
 from DailySales
 group by date_id, make_name
+order by date_id, make_name
