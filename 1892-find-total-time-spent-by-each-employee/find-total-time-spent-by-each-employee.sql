04826a0904626f493dc0de4df453779e942331bb
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-07-07 12:25:45 +0700

    Time: 282 ms (63.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1892-find-total-time-spent-by-each-employee/find-total-time-spent-by-each-employee.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+select event_day as day, emp_id, sum(out_time-in_time) as total_time
+from Employees
+group by event_day, emp_id
+order by emp_id, total_time desc
