3a3791590ca99242ee52acc567fd1f151ed10778
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2024-12-12 16:40:03 +0700

    Time: 969 ms (77.59%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+select distinct p.product_id, p.product_name
+from Product p
+join Sales s on s.product_id = p.product_id
+where s.sale_date between '2019-01-01' and '2019-03-31' 
+and p.product_id not in (select distinct product_id from Sales
+where sale_date < '2019-01-01' or sale_date > '2019-03-31')
