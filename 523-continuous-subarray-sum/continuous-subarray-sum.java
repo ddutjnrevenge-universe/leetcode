8ad1fea7d8c3db34ed6d0bdb916332007337ff2e
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2025-03-27 22:54:25 +0700

    Time: 24 ms (71.64%) | Memory: 57.6 MB (76.61%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer, Integer> remainderMap = new HashMap<>();
+        int cumulativeSum = 0;
+    
+        remainderMap.put(0, -1);
+    
+        for (int i = 0; i < n; i++) {
+            cumulativeSum += nums[i];
+            
+            int remainder = k == 0 ? cumulativeSum : cumulativeSum % k;
+            
+            if (remainderMap.containsKey(remainder)) {
+
+                if (i - remainderMap.get(remainder) > 1) {
+                    return true;
+                }
+            } else {
+                remainderMap.put(remainder, i);
+            }
+        }
+    
+        return false;
+    }
+}
