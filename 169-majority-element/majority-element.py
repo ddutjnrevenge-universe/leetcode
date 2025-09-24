ef86fd1d7fce92756109c9a1c0d955b2317da101
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2025-09-25 06:38:54 +0700

    Time: 79 ms (5.91%) | Memory: 14.7 MB (6.96%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,6 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return max(set(nums), key = nums.count)
+        def helper(nums,left,right):
+            if left==right:
+                return nums[left]
+            mid=(left+right)//2
+            left_maj = helper(nums,left,mid)
+            right_maj = helper(nums,mid+1,right)
 
+            if left_maj == right_maj: 
+                return left_maj
+            left_count=right_count=0
+            for i in range(left,right+1):
+                if nums[i]==left_maj:
+                    left_count+=1
+                if nums[i]==right_maj:
+                    right_count+=1
+
+            if left_count > (right-left+1)//2:
+                return left_maj
+            elif right_count > (right-left+1)//2:
+                return right_maj
+            else:
+                return None
+        return helper(nums,0,len(nums)-1)
         
