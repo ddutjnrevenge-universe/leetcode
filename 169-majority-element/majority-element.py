13923c6916ab0dd1a9b86cc37cc118d215ed768e
Hanh Nguyen-Tran <128204594+ddutjnrevenge-universe@users.noreply.github.com>
2025-10-06 22:33:18 +0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (50.40%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,30 +1,5 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        def helper(nums,left,right):
-            if left==right:
-                return nums[left]
-            mid=(left+right)//2
-            left_maj = helper(nums,left,mid)
-            right_maj = helper(nums,mid+1,right)
-
-            if left_maj == right_maj: 
-                return left_maj
-            left_count=right_count=0
-            for i in range(left,right+1):
-                if nums[i]==left_maj:
-                    left_count+=1
-                if nums[i]==right_maj:
-                    right_count+=1
-
-            if left_count > (right-left+1)//2:
-                return left_maj
-            elif right_count > (right-left+1)//2:
-                return right_maj
-            else:
-                return None
-        return helper(nums,0,len(nums)-1)
-        
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        n=len(nums)
+        return nums[n//2]
